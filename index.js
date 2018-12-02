8940ada967b3fc7d92e34a4c1582b068359d63a3
Jon Preece <jonpreece@Jons-MacBook-Pro.local>
2018-12-02 07:11:08 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+mongoose.connect(
+  'mongodb://root:example@localhost:27017/test?authSource=admin&w=1',
+  { useNewUrlParser: true }
+)
+
+const Cat = mongoose.model('Cat', { name: String })
+
+// const kitty = new Cat({ name: 'Zildjian' });
+// kitty.save().then(() => console.log('meow'));
+
+Cat.find({ name: 'Zildjian' }, (err, cats) => {
+  console.log(cats)
+})
